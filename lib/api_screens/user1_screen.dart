8a509e59fbce0231ad1ecc8e5f9bc425578940b1
Abidcomputers <ym399758@gmail.com>
2025-01-19 03:45:09 -0800

    User api with model

--- lib/api_screens/user1_screen.dart
+++ lib/api_screens/user1_screen.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:smit_api/models/user1_model/user1_model.dart';
 class User1Screen extends StatefulWidget {
   const User1Screen({super.key});
 
@@ -11,13 +12,25 @@ class User1Screen extends StatefulWidget {
 }
 
 class _User1ScreenState extends State<User1Screen> {
+  List<User1Model> list = [];
   
   var url = Uri.parse('https://jsonplaceholder.typicode.com/users');
-  getApiUser1()async{
+  Future<List<User1Model>>getApiUser1()async{
     final response = await http.get(url);
-    final responseBody = jsonDecode(response.body);
-    print(responseBody);
-    return responseBody;
+    if(response.statusCode == 200){
+      final responseBody = jsonDecode(response.body);
+      for(var value in responseBody){
+        list.add(User1Model.fromMap(value));
+      }
+    }
+    return list;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getApiUser1();
   }
   
   @override
@@ -27,25 +40,18 @@ class _User1ScreenState extends State<User1Screen> {
         backgroundColor: Colors.blue,
         title: Text('Users API'),
       ),
-      body: FutureBuilder(
-          future: getApiUser1(),
-          builder: (context,AsyncSnapshot snapshot){
-            if(snapshot.hasData){
-              return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context,index){
-                    return ListTile(
-                      leading: Text(index.toString()),
-                      title: Text(snapshot.data[index]['name'].toString()),
-                      subtitle: Text(snapshot.data[index]['username'].toString()),
-                      trailing: Text(snapshot.data[index]['company']['name'].toString()),
-                    );
-                  });
-            }
-            else{
-              return Center(child: CircularProgressIndicator());
-            }
-          }),
+      body: list.isEmpty ? Center(child: CircularProgressIndicator()) : ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context,index){
+        return ListTile(
+          leading: CircleAvatar(
+             backgroundColor: Colors.amber,
+              child: Text(index.toString())),
+          title: Text(list[index].name.toString()),
+          subtitle: Text(list[index].phone.toString()),
+          trailing: Text(list[index].address!.street.toString()),
+        );
+      })
     );
   }
 }
